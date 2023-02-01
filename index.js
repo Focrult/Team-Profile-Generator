3e35d8177a3b3fc4f714cc6abfdd75bfabcb55ca
Focrult <focrult@gmail.com>
2023-02-01 20:32:01 +0800

    Fixed an issue with promptsgit add .git add .! Took advantage of functions and now prompts will repeat until user selects done

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const TeamManager = [ //should initiate first!
         message: 'Enter your name',
         },
         {
-        type: 'input',
+        type: 'number',
         name: 'id',
         message: 'Enter your id',
         },
@@ -29,31 +29,34 @@ const TeamManager = [ //should initiate first!
         message: 'Enter your email address',
         },
         {
-        type: 'input',
+        type: 'number',
         name: 'officeNumber',
         message: 'Enter your office number',
         },
-    ]).then(Info => {
-        const managerInfo = new Manager(Info.name, Info.id, Info.email, Info.OfficeNumber);
+    ]).then((info) => {
+        const managerInfo = new Manager(info.name, info.id, info.email, info.OfficeNumber);
         TeamArrayInfo.push(managerInfo); //removed this. <- didn't work
         roles(); //activate roles function
      
     })
 ]
 }
+function reviewAnswer(){
+    roles();
+
+}
 function roles() {
 //activating prompts for the user to select - roles
-const roles = [
+const rolesprompt = [
     inquirer.prompt([
         {
             type: 'list',
             message: 'What role would you like to select?',
-            choices: ["Intern", "Engineer", "Done!"],
+            choices: ['Intern', 'Engineer', 'Done!'],
             name: 'roles'
         }
-    ]).then(({roles}) => {
-        // const selected = {roles};
-        if(roles === "Intern"){
+    ]).then(({roles}) => { 
+        if(roles === 'Intern'){
             //prompt for the user to select
             inquirer.prompt([
                 {
@@ -62,7 +65,7 @@ const roles = [
                     message: 'Enter the name of your intern: ',
                 },
                 {
-                    type: 'input',
+                    type: 'number',
                     name: 'id',
                     message: 'Enter the id for your intern: ',
                 },
@@ -76,10 +79,11 @@ const roles = [
                     name: 'school',
                     message: 'What school is your intern from?: ',
                 },
-            ]).then(Info => {
-                const internInfo = new Intern(Info.name, Info.id, Info.email, Info.school);
+            ]).then((info) => {
+                const internInfo = new Intern(info.name, info.id, info.email, info.school);
                 TeamArrayInfo.push(internInfo);//push into an array?
-                return inquirer.prompt(roles);
+                reviewAnswer();
+                //return inquirer.prompt(rolesprompt);
     //create a return for prompts?
             })
         }
@@ -92,7 +96,7 @@ const roles = [
                     message: 'Enter the name of your engineer: ',
                 },
                 {
-                    type: 'input',
+                    type: 'number',
                     name: 'id',
                     message: 'Enter the id for your engineer: ',
                 },
@@ -106,10 +110,10 @@ const roles = [
                     name: 'github',
                     message: 'Enter the github for your engineer: ',
                 },
-            ]).then(Info => { //push into an array?
-                const engineerInfo = new Engineer(Info.name, Info.email, Info.github, info.id);
+            ]).then((info) => { //push into an array?
+                const engineerInfo = new Engineer(info.name, info.email, info.github, info.id);
                 TeamArrayInfo.push(engineerInfo); 
-                return inquirer.prompt(roles);
+                return inquirer.prompt(rolesprompt);
             })
         }
         else{
