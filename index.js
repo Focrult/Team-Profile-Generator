30d77abab378d95b868c0eecd4c334af9a3a1e7a
Focrult <focrult@gmail.com>
2023-02-01 10:39:19 +0800

    Added prompts for the user. Additionally planned out how I would further tackle the challenge. Created relevant files for html creation

--- index.js
+++ index.js
@@ -1,6 +1,35 @@
 const inquirer = require('inquirer');
 
-//html markup?
+const TeamArrayInfo = require('./src/generatedHTML');  //file used to apply teamArray to the html
+//TEAM MANAGER
+const TeamManager = [ //should initiate first!
+    inquirer.prompt([
+        {
+        type: 'input',
+        name: 'name',
+        message: 'Enter your name',
+        },
+        {
+        type: 'input',
+        name: 'id',
+        message: 'Enter your id',
+        },
+        {
+        type: 'input',
+        name: 'email',
+        message: 'Enter your email address',
+        },
+        {
+        type: 'input',
+        name: 'officeNumber',
+        message: 'Enter your office number',
+        },
+    ]).then(pushInfo => {
+        const managerInfo = new Manager();
+        this.TeamArrayInfo.push(managerInfo);
+        this.roles();
+    })
+]
 
 //activating prompts for the user to select - roles
 const roles = [
@@ -14,8 +43,58 @@ const roles = [
         }
     ]).then(({roles}) => {
         const selected = {roles};
-        const transfer = JNBVBIHJSBV(selected);
-        console.log(transfer);
+        if(roles === "Intern"){
+            //prompt for the user to select
+            inquirer.prompt([
+                {
+                    type: 'input',
+                    name: 'name',
+                    message: 'Enter the name of your intern: ',
+                },
+                {
+                    type: 'input',
+                    name: 'id',
+                    message: 'Enter the id for your intern: ',
+                },
+                {
+                    type: 'input',
+                    name: 'email',
+                    message: 'Enter the email of your intern: ',
+                },
+            ]).then(pushInfo => {
+                const internInfo = new Intern();
+                this.TeamArrayInfo.push(internInfo);//push into an array?
+    
+            })
+        }
         //write to html?
+
+        else if(roles === "Engineer"){
+             //prompt for the user to select
+             inquirer.prompt([
+                {
+                    type: 'input',
+                    name: 'name',
+                    message: 'Enter the name of your engineer: ',
+                },
+                {
+                    type: 'input',
+                    name: 'id',
+                    message: 'Enter the id for your engineer: ',
+                },
+                {
+                    type: 'input',
+                    name: 'email',
+                    message: 'Enter the email of your engineer: ',
+                },
+            ]).then(pushInfo => { //push into an array?
+                const engineerInfo = new Engineer();
+                this.TeamArrayInfo.push(engineerInfo);
+    
+            })
+        }
+        else if(roles === "Done!"){
+            generatedHTML(teamArray); //pass the array to the html generator
+        }
     })
 ]
